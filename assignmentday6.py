ed470e8fa1723a9936c1768c28e8ca1f4261cabe
afreen08 <71078194+afreen08@users.noreply.github.com>
2020-09-12 19:54:36 +0530

    Add files via upload

---
+++ assignmentday6.py
@@ -0,0 +1,39 @@
+#create class
+class bank():
+    def __init__(self,ownername,balance):
+        self.ownername=ownername
+        self.balance
+    def deposit(self,det_amt):
+        self.balance+=det_amt
+        return("deposited")
+    def withdraw(self,withdraw):
+        if self.balance>=withdraw:
+            self.balance-=withdraw
+            return("withdraw accepted")
+        else:
+            print("not upto u r balance")
+a=bank("asifa",2000)
+print(a.deposit(100))
+print(a.withdraw(300))
+
+
+
+#cone
+import math
+class cone():
+    def __init__(self,r,h):
+        self.r=r
+        self.h=h
+    def volume(self):
+        b=3.14*self.r*self.r*(self.h/3)
+        return(b)
+    def surfacearea(self):
+        c=3.14*self.r*self.r
+        return(c)
+    def side (self):
+        d=3.14*self.r*math.sqrt(self.r*self.r+self.h*self.h)
+        return(d)
+p=cone(1,2)
+print(p.volume())
+print(p.surfacearea())
+print(p.side())
